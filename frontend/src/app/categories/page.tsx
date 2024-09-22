4710d92edae2cfaf7a3dd091d4b83386246df2b2
AceGjorgjievski <ace.gjorgjievski6@gmail.com>
2024-09-22 14:52:04 +0200

    frontend setup

---
+++ frontend/src/app/categories/page.tsx
@@ -0,0 +1,13 @@
+
+
+const Categories = () => {
+    return (
+        <>
+            <main>
+                <h2>Welcome to categories page</h2>
+            </main>
+        </>
+    );
+};
+
+export default Categories;
---
+++ frontend/src/app/utils/categories.utils.tsx
@@ -0,0 +1,50 @@
+
+const DOMAIN_NAME = "http://localhost:";
+const PORT = 4000;
+const FULL_DOMAIN = DOMAIN_NAME + +PORT;
+const CATEGORIES_URL = "https://www.themealdb.com/api/json/v1/1/list.php?c=list";
+const BACKEND_URL_CATEGORIES = FULL_DOMAIN + "/categories";
+
+
+export const getCategoriesFromApi = async () => {
+
+    const response = await fetch(CATEGORIES_URL);
+
+    const data = await response.json();
+
+    const allCategories = data.meals.map((category: {strCategory: string}, index: number) => {
+        return {id: index + 1, strCategory: category.strCategory}
+    })
+
+    return allCategories;
+}
+
+export const saveCategories = async () => {
+    const allCategories = await getCategoriesFromApi();
+
+    for(const category of allCategories) {
+        await fetch(BACKEND_URL_CATEGORIES + "/add", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(category)
+        })
+    }
+
+    console.log("categories saved in db");
+
+}
+
+export const getCategoriesFromDb = async () => {
+    const response = await fetch(BACKEND_URL_CATEGORIES, {
+        method: 'GET',
+        cache: 'no-cache'
+    });
+    const data = await response.json();
+
+    return data;
+}
+
+
+
