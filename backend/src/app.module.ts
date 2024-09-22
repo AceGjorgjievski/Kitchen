e5406267ff35dfef0525e56058b0a1ac744e69a0
AceGjorgjievski <ace.gjorgjievski6@gmail.com>
2024-09-22 14:49:40 +0200

    initial setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoryModule } from './categories/categories.module';
+import { MealsModule } from './meals/meals.module';
+import { FirestoreModule } from './firestore/firestore.module';
 
 @Module({
-  imports: [],
+  imports: [CategoryModule, MealsModule, FirestoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { initializeFirebase } from './firebase/firebase.config';
+
+dotenv.config();
 
 async function bootstrap() {
+
+  initializeFirebase();
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
