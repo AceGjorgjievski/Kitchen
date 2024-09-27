4cc3c0cc50ebfad97e7d46f564afc73527ece170
AceGjorgjievski <ace.gjorgjievski6@gmail.com>
2024-09-27 12:36:16 +0200

    imported AuthModule in the AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { CategoryModule } from './categories/categories.module';
 import { MealsModule } from './meals/meals.module';
 import { FirestoreModule } from './firestore/firestore.module';
+import {AuthModule} from "./auth/auth.module";
 
 @Module({
-  imports: [CategoryModule, MealsModule, FirestoreModule],
+  imports: [CategoryModule, MealsModule, FirestoreModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
