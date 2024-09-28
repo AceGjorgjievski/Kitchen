73984a0e2c38e32939d13fbe28606bda0cb85444
AceGjorgjievski <ace.gjorgjievski6@gmail.com>
2024-09-28 14:25:34 +0200

    app.module.ts modification

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,31 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CategoryModule } from './categories/categories.module';
-import { MealsModule } from './meals/meals.module';
-import { FirestoreModule } from './firestore/firestore.module';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {CategoryModule} from './categories/categories.module';
+import {MealsModule} from './meals/meals.module';
+import {FirestoreModule} from './firestore/firestore.module';
 import {AuthModule} from "./auth/auth.module";
+import {APP_GUARD, Reflector} from "@nestjs/core";
+import {AuthGuardService} from "./auth/guards/authGuard.service";
+import {FirebaseModule} from "./firebase/firebase.module";
+import {ConfigModule} from "@nestjs/config";
+import {UsersModule} from "./users/users.module";
 
 @Module({
-  imports: [CategoryModule, MealsModule, FirestoreModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [CategoryModule, MealsModule, FirestoreModule,
+        AuthModule, FirebaseModule, ConfigModule.forRoot({
+            isGlobal: true
+        }),
+        UsersModule
+    ],
+    controllers: [AppController],
+    providers: [AppService,
+        {
+            provide: APP_GUARD,
+            useClass: AuthGuardService
+        },
+        Reflector
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
